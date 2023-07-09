fb6bf90f63287d3813e1efcc2cf9ea860f8a0031
Humanesotf <2997207238@qq.com>
2023-07-10 01:28:31 +0800

    fist try

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Humanesoft/Test
+
+go 1.20
---
+++ sum.go
@@ -0,0 +1,13 @@
+package calculator
+
+var logMessage = "[LOG]"
+
+var Version = "1.0"
+
+func internalSum(number int) int {
+	return number - 1
+}
+
+func Sum(number1, nuber2 int) int {
+	return number1 + nuber2
+}
